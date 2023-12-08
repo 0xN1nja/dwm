a40d8acc499a8761024fd04dc2354e9fb2424db5
Abhimanyu Sharma <speedcuberabhi@gmail.com>
2023-12-08 11:27:02 +0530

    Updated config.h

--- config.h
+++ config.h
@@ -95,6 +95,8 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_e,      spawn,          SHCMD("emacs")},
+	{ MODKEY|ShiftMask,             XK_b,      spawn,          SHCMD("firefox-bin")},
 };
 
 /* button definitions */
